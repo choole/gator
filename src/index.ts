6080b31b9388195213335b8036abe1f1965365a3
choole <nemanjacukic31@gmail.com>
2026-05-23 00:21:25 +0200

    Add: impl users and rss feeds table and their commands

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { type CommandsRegistry, registerCommand, runCommand } from "./commands/command";
+import { handlerLogin, handlerRegister, handlerListUsers } from "./commands/users";
+import { handlerReset } from "./commands/reset";
+import { handlerAddFeed, handlerListFeeds } from "./commands/rss";
+
+async function main() {
+	const args = process.argv.slice(2);
+
+	if (args.length < 1) {
+		console.log("usage: cli <command> [args...]");
+		process.exit(1);
+	}
+
+	const cmdName = args[0];
+	const cmdArgs = args.slice(1);
+	const commandsRegistry: CommandsRegistry = {};
+
+	registerCommand(commandsRegistry, "login", handlerLogin);
+	registerCommand(commandsRegistry, "register", handlerRegister);
+	registerCommand(commandsRegistry, "reset", handlerReset);
+	registerCommand(commandsRegistry, "users", handlerListUsers);
+	registerCommand(commandsRegistry, "addfeed", handlerAddFeed);
+	registerCommand(commandsRegistry, "feeds", handlerListFeeds);
+
+	try {
+		await runCommand(commandsRegistry, cmdName, ...cmdArgs);
+	} catch (err) {
+		if (err instanceof Error) {
+			console.error(`Error running command ${cmdName}: ${err.message}`);
+		} else {
+			console.error(`Error running command ${cmdName}: ${err}`);
+		}
+		process.exit(1);
+	}
+	process.exit(0);
+}
+
+main();
